d7799371e37a94e2338427eb642bb7f188bcf268
Andrea Grandi <andrea.grandi@tiscali.it>
2025-03-24 11:03:33 +0100

    update model

--- inference.py
+++ inference.py
@@ -1,14 +1,13 @@
 import time
 
 import numpy as np
+import torch
 import onnxruntime as ort
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 # Load the tokenizer
-tokenizer = AutoTokenizer.from_pretrained("google/mobilebert-uncased")
-
-# Load the ONNX model
-ort_session = ort.InferenceSession("./models/mobilebert_sentiment.onnx")
+tokenizer = AutoTokenizer.from_pretrained("./tokenizer/")
+model = AutoModelForSequenceClassification.from_pretrained("./models/")
 
 # Map indices back to emotion labels
 id_to_emotion = {
@@ -24,32 +23,25 @@ id_to_emotion = {
 def classify_text(text):
     # Preprocess text
     inputs = tokenizer(
-        text, return_tensors="np", truncation=True, padding="max_length", max_length=128
+        text, return_tensors="pt", truncation=True, padding="max_length", max_length=128
     )
-
-    input_ids = np.array(inputs["input_ids"], dtype=np.int64)
-    # print(input_ids)
-
-    # Convert to NumPy arrays for ONNX Runtime
-    ort_inputs = {"input_ids": input_ids}
-
-    # print(ort_inputs)
+    
     # Run inference
     start_time = time.time()
-    ort_outputs = ort_session.run(None, ort_inputs)
+    with torch.no_grad():
+        outputs = model(**inputs)
     end_time = time.time()
-
+    
     # Get predictions
-    logits = ort_outputs[0]
-    predicted_class = np.argmax(logits, axis=1)[0]
+    logits = outputs.logits
+    predicted_class = torch.argmax(logits, dim=1).item()
     predicted_emotion = id_to_emotion[predicted_class]
-
+    
     print(f"Text: {text}")
     print(f"Predicted emotion: {predicted_emotion}")
     print(f"Inference time: {(end_time - start_time) * 1000:.2f} ms")
-
+    
     return predicted_emotion
-
 
 if __name__ == "__main__":
     sample_text = "Ti odio!!"
